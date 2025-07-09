12c6f17f459bcfd48ff31b1f5d6bab2f9cfe7798
mmsqe <tqd0800210105@gmail.com>
2025-07-09 09:33:30 +0800

    expect deposit is too large

--- integration_tests/test_patch.py
+++ integration_tests/test_patch.py
@@ -1,10 +1,8 @@
-import time
 from pathlib import Path
 
 import pytest
 
 from .network import setup_custom_mantra
-from .utils import wait_for_new_blocks
 
 
 @pytest.fixture(scope="module")
@@ -13,6 +11,7 @@ def custom_mantra(tmp_path_factory):
     yield from setup_custom_mantra(
         path, 26700, Path(__file__).parent / "configs/patch.jsonnet"
     )
+
 
 @pytest.mark.skip(reason="skipping integer overflow test")
 def test_int_overflow(custom_mantra):
@@ -25,7 +24,7 @@ def test_int_overflow(custom_mantra):
 
     # fund validator rewards pool
     denom = "utesttest"
-    delegation_amt_w_denom = f"115792089237316195423570985008687907853269984665640564039457584007913129639935{denom}"
+    delegation_amt_w_denom = f"115792089237316195423570985008687907853269984665640564039457584007913129639935{denom}"  # noqa: E501
     rsp = cli.fund_validator_rewards_pool(
         val_addr,
         delegation_amt_w_denom,
@@ -51,11 +50,5 @@ def test_int_overflow(custom_mantra):
         f"{balance}{denom}",
         from_=name,
     )
-    assert rsp["code"] == 0, rsp["raw_log"]
-    balance = cli.balance(bech32_addr, denom=denom)
-
-    # stop the validator to trigger slashing due to validator down
-    custom_mantra.supervisorctl("stop", "mantra-canary-net-1-node0")
-    time.sleep(5)
-    custom_mantra.supervisorctl("start", "mantra-canary-net-1-node0")
-    wait_for_new_blocks(cli, 2, timeout=30)
+    assert rsp["code"] != 0
+    assert "deposit is too large: Int overflow" in rsp["raw_log"]
