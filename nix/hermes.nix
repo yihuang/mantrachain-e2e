e0e9617be5b2df39860202a369f106723a66f26a
mmsqe <tqd0800210105@gmail.com>
2025-08-09 17:53:37 +0800

    Problem: not using latest hermes in ibc test (#34)
    * Problem: not using latest hermes in ibc test
    
    bump deps
    
    * align nixos
    
    * fix ci
    
    * less resource for connect
    
    * debug
    
    * try sandbox

--- nix/hermes.nix
+++ nix/hermes.nix
@@ -3,20 +3,29 @@
   lib,
   stdenv,
   darwin,
-  rustPlatform,
+  rustPackages_1_83,
   symlinkJoin,
   openssl,
   pkg-config,
+  protobuf,
+  clang,
+  llvmPackages,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPackages_1_83.rustPlatform.buildRustPackage rec {
   name = "hermes";
   inherit src;
-  cargoBuildFlags = "-p ibc-relayer-cli";
-  buildInputs = lib.optionals stdenv.isDarwin [
-    darwin.apple_sdk.frameworks.Security
+  cargoBuildFlags = [ "-p" "ibc-relayer-cli" ];
+  nativeBuildInputs = [
     pkg-config
+    protobuf
+    clang
+  ];
+  buildInputs = [
     openssl
+    llvmPackages.libclang.lib
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Security
     darwin.libiconv
     darwin.apple_sdk.frameworks.SystemConfiguration
   ];
@@ -24,13 +33,17 @@ rustPlatform.buildRustPackage rec {
     lockFile = "${src}/Cargo.lock";
   };
   doCheck = false;
-  RUSTFLAGS = "--cfg ossl111 --cfg ossl110 --cfg ossl101";
-  OPENSSL_NO_VENDOR = "1";
-  OPENSSL_DIR = symlinkJoin {
-    name = "openssl";
-    paths = with openssl; [
-      out
-      dev
-    ];
+  env = {
+    RUSTFLAGS = "--cfg ossl111 --cfg ossl110 --cfg ossl101";
+    OPENSSL_NO_VENDOR = "1";
+    PROTOC = "${protobuf}/bin/protoc";
+    LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+    OPENSSL_DIR = symlinkJoin {
+      name = "openssl";
+      paths = with openssl; [
+        out
+        dev
+      ];
+    };
   };
 }
