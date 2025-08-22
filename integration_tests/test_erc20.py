91858a6a9abee03472f55efb9f68806f9995c258
mmsqe <tqd0800210105@gmail.com>
2025-08-22 21:21:44 +0800

    register another weth

--- integration_tests/test_erc20.py
+++ integration_tests/test_erc20.py
@@ -22,13 +22,13 @@ async def test_static_erc20(mantra, tmp_path):
     w3 = mantra.async_w3
     account = ACCOUNTS["community"]
     await ensure_create2_deployed(w3, account)
-    await ensure_deployed_by_create2(
+    weth_addr = await ensure_deployed_by_create2(
         w3,
         account,
         get_initcode(WETH9_ARTIFACT),
-        salt=WETH_SALT,
+        salt=WETH_SALT - 100,
     )
-
+    assert weth_addr != WETH_ADDRESS, "should be different weth address"
     owner = account.address
     submit_gov_proposal(
         mantra,
@@ -37,36 +37,38 @@ async def test_static_erc20(mantra, tmp_path):
             {
                 "@type": "/cosmos.evm.erc20.v1.MsgRegisterERC20",
                 "signer": module_address("gov"),
-                "erc20addresses": [WETH_ADDRESS],
+                "erc20addresses": [weth_addr],
             },
         ],
         gas=300000,
     )
     cli = mantra.cosmos_cli()
-    cli.query_erc20_token_pairs()
+    erc20_denom = f"erc20:{weth_addr}"
+    res = cli.query_erc20_token_pair(erc20_denom)
+    assert res["erc20_address"] == weth_addr, res
     # deposit should be nop
     before = await w3.eth.get_balance(owner)
-    weth = WETH(to=WETH_ADDRESS)
+    weth = WETH(to=weth_addr)
     before = await balance_of(w3, ZERO_ADDRESS, owner)
     receipt = await weth.fns.deposit().transact(w3, account, value=1000)
     fee = receipt["effectiveGasPrice"] * receipt["gasUsed"]
-    await balance_of(w3, WETH_ADDRESS, owner) == 1000
+    await balance_of(w3, weth_addr, owner) == 1000
     receipt = await weth.fns.withdraw(1000).transact(w3, account)
     fee += receipt["effectiveGasPrice"] * receipt["gasUsed"]
-    await balance_of(w3, WETH_ADDRESS, owner) == 0
+    await balance_of(w3, weth_addr, owner) == 0
 
     # withdraw should be nop
-    weth = WETH(to=WETH_ADDRESS)
+    weth = WETH(to=weth_addr)
     before = await balance_of(w3, ZERO_ADDRESS, owner)
     receipt = await weth.fns.deposit().transact(w3, account, value=1000)
     fee = receipt["effectiveGasPrice"] * receipt["gasUsed"]
-    await balance_of(w3, WETH_ADDRESS, owner) == 1000
+    await balance_of(w3, weth_addr, owner) == 1000
     receipt = await weth.fns.withdraw(1000).transact(w3, account)
     fee += receipt["effectiveGasPrice"] * receipt["gasUsed"]
-    await balance_of(w3, WETH_ADDRESS, owner) == 0
+    await balance_of(w3, weth_addr, owner) == 0
     assert await balance_of(w3, ZERO_ADDRESS, owner) == before - fee
 
     # fail
-    assert await ERC20.fns.decimals().call(w3, to=WETH_ADDRESS) == 18
-    assert await ERC20.fns.symbol().call(w3, to=WETH_ADDRESS) == "WETH"
-    assert await ERC20.fns.name().call(w3, to=WETH_ADDRESS) == "Wrapped Ether"
+    assert await ERC20.fns.decimals().call(w3, to=weth_addr) == 18
+    assert await ERC20.fns.symbol().call(w3, to=weth_addr) == "WETH"
+    assert await ERC20.fns.name().call(w3, to=weth_addr) == "Wrapped Ether"
