6cfb615c609d8ffb7164b651d514017b1d1f036b
mmsqe <tqd0800210105@gmail.com>
2025-08-29 09:48:18 +0800

    Problem: no exception retry config

--- integration_tests/network.py
+++ integration_tests/network.py
@@ -7,8 +7,14 @@ from pathlib import Path
 import tomlkit
 import web3
 from pystarport import cluster, ports
-from web3 import AsyncHTTPProvider, AsyncWeb3
+from requests.exceptions import (
+    HTTPError,
+    Timeout,
+    TooManyRedirects,
+)
+from web3 import AsyncHTTPProvider, AsyncWeb3, HTTPProvider, WebSocketProvider
 from web3.middleware import ExtraDataToPOAMiddleware
+from web3.providers.rpc.utils import ExceptionRetryConfiguration
 
 from .cosmoscli import CosmosCLI
 from .utils import (
@@ -16,6 +22,11 @@ from .utils import (
     wait_for_block,
     wait_for_port,
     wait_for_url,
+)
+
+RETRY_CONFIG = ExceptionRetryConfiguration(
+    errors=(ConnectionError, HTTPError, Timeout, TooManyRedirects),
+    retries=10,
 )
 
 
@@ -53,13 +64,25 @@ class Mantra:
 
     def node_w3(self, i=0):
         if self._use_websockets:
-            return web3.Web3(web3.providers.WebsocketProvider(self.w3_ws_endpoint(i)))
+            return web3.Web3(
+                WebSocketProvider(
+                    self.w3_ws_endpoint(i), exception_retry_configuration=RETRY_CONFIG
+                )
+            )
         else:
-            return web3.Web3(web3.providers.HTTPProvider(self.w3_http_endpoint(i)))
+            return web3.Web3(
+                HTTPProvider(
+                    self.w3_http_endpoint(i), exception_retry_configuration=RETRY_CONFIG
+                )
+            )
 
     def async_node_w3(self, i=0):
         return AsyncWeb3(
-            AsyncHTTPProvider(self.w3_http_endpoint(i), cache_allowed_requests=True)
+            AsyncHTTPProvider(
+                self.w3_http_endpoint(i),
+                cache_allowed_requests=True,
+                exception_retry_configuration=RETRY_CONFIG,
+            ),
         )
 
     def base_port(self, i):
@@ -114,12 +137,24 @@ class ConnectMantra:
 
     def node_w3(self):
         if self._use_websockets:
-            return web3.Web3(web3.providers.WebsocketProvider(self.evm_rpc_ws))
+            return web3.Web3(
+                WebSocketProvider(
+                    self.evm_rpc_ws, exception_retry_configuration=RETRY_CONFIG
+                )
+            )
         else:
-            return web3.Web3(web3.providers.HTTPProvider(self.evm_rpc))
+            return web3.Web3(
+                HTTPProvider(self.evm_rpc, exception_retry_configuration=RETRY_CONFIG)
+            )
 
     def async_node_w3(self):
-        return AsyncWeb3(AsyncHTTPProvider(self.evm_rpc, cache_allowed_requests=True))
+        return AsyncWeb3(
+            AsyncHTTPProvider(
+                self.evm_rpc,
+                cache_allowed_requests=True,
+                exception_retry_configuration=RETRY_CONFIG,
+            )
+        )
 
     def cosmos_cli(self, home) -> CosmosCLI:
         return CosmosCLI(home, self.rpc, self.chain_binary, self.chain_id)
@@ -221,7 +256,7 @@ def setup_geth(path, base_port):
         try:
             wait_for_port(base_port)
             url = f"http://127.0.0.1:{base_port}"
-            w3 = web3.Web3(web3.providers.HTTPProvider(url))
+            w3 = web3.Web3(HTTPProvider(url))
             w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
             async_w3 = AsyncWeb3(AsyncHTTPProvider(url, cache_allowed_requests=True))
             async_w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
