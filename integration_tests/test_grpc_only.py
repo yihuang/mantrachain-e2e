2f5adf5c7ca3050ceb02f5991a1f44fec21193c1
mmsqe <tqd0800210105@gmail.com>
2025-11-13 09:12:46 +0800

    Problem: grpc_only flow is not cleanup

--- integration_tests/test_grpc_only.py
+++ integration_tests/test_grpc_only.py
@@ -5,20 +5,20 @@ from pathlib import Path
 import pytest
 from pystarport import ports
 from pystarport.utils import wait_for_block, wait_for_port
-from web3._utils.contracts import encode_transaction_data
 
 from .network import setup_custom_mantra
 from .utils import (
     CHAIN_ID,
     CMD,
     EVM_CHAIN_ID,
-    Contract,
+    AsyncContract,
     decode_bech32,
     grpc_eth_call,
     supervisorctl,
 )
 
 pytest.skip("wait next bump deps", allow_module_level=True)
+pytestmark = pytest.mark.asyncio
 
 
 @pytest.fixture(scope="module")
@@ -33,34 +33,34 @@ def custom_mantra(request, tmp_path_factory):
     )
 
 
-def test_grpc_mode(custom_mantra):
-    w3 = custom_mantra.w3
-    contract = Contract("ChainID")
-    data = encode_transaction_data(
-        w3, "currentChainID", contract.abi, args=[], kwargs={}
-    )
-    contract.deploy(w3)
+async def test_grpc_mode(custom_mantra):
+    w3 = custom_mantra.async_w3
+    contract = AsyncContract("ChainID")
+    await contract.deploy(w3)
+    to = contract.address
     contract = contract.contract
+    data = contract.fns.currentChainID().data.hex()
+    msg = {"to": to, "data": f"0x{data}"}
+    base_port = custom_mantra.base_port(1)
+    api_port = ports.api_port(base_port)
+    grpc_port = ports.grpc_port(base_port)
 
-    msg = {
-        "to": contract.address,
-        "data": data,
-    }
-    api_port = ports.api_port(custom_mantra.base_port(1))
-
-    def expect_cb(rsp):
+    def expect_cb_normal(rsp):
         ret = rsp.get("ret")
-        valid = ret is not None
-        return valid and EVM_CHAIN_ID == int.from_bytes(
-            base64.b64decode(ret.encode()), "big"
-        )
+        if ret is None:
+            return False
+        return EVM_CHAIN_ID == int.from_bytes(base64.b64decode(ret.encode()), "big")
+
+    grpc_eth_call(api_port, msg, expect_cb_normal)
 
     # in normal mode, grpc query works even if we don't pass chain_id explicitly
-    grpc_eth_call(api_port, msg, expect_cb)
+    grpc_eth_call(api_port, msg, expect_cb_normal)
+
     # wait 1 more block for both nodes to avoid node stopped before tnx get included
-    base_dir = custom_mantra.base_dir
     for i in range(2):
         wait_for_block(custom_mantra.cosmos_cli(i), 1)
+
+    base_dir = custom_mantra.base_dir
     supervisorctl(base_dir / "../tasks.ini", "stop", f"{CHAIN_ID}-node1")
 
     # run grpc-only mode directly with existing chain state
@@ -77,30 +77,29 @@ def test_grpc_mode(custom_mantra):
             stderr=subprocess.STDOUT,
         )
         try:
-            # wait for grpc and rest api ports
-            grpc_port = ports.grpc_port(custom_mantra.base_port(1))
-            wait_for_port(grpc_port)
-            wait_for_port(api_port)
-
-            def expect_cb(rsp):
-                return "code" not in rsp
-
-            # it defaults to empty address without proposer address
-            grpc_eth_call(api_port, msg, expect_cb, chain_id=EVM_CHAIN_ID)
+            for port in (grpc_port, api_port):
+                wait_for_port(port)
 
             # pass the first validator's consensus address to grpc query
+            grpc_eth_call(
+                api_port, msg, lambda rsp: "code" not in rsp, chain_id=EVM_CHAIN_ID
+            )
+
+            # Get consensus address and prepare more strict expect_cb
             addr = custom_mantra.cosmos_cli(0).consensus_address()
             cons_addr = decode_bech32(addr)
 
-            def expect_cb(rsp):
+            def expect_cb_strict(rsp):
+                if "code" in rsp:
+                    return False
                 ret = base64.b64decode(rsp["ret"].encode())
-                return "code" not in rsp and EVM_CHAIN_ID == int.from_bytes(ret, "big")
+                return EVM_CHAIN_ID == int.from_bytes(ret, "big")
 
             # should work with both chain_id and proposer_address set
             grpc_eth_call(
                 api_port,
                 msg,
-                expect_cb,
+                expect_cb_strict,
                 chain_id=100,
                 proposer_address=base64.b64encode(cons_addr).decode(),
             )
